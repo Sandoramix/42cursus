3a761050c78330c5292ee221dec8a420408d4f7b
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-09 17:48:21 +0200

    ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,7 +11,45 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+static size_t	ft_get_n_size(int n)
+{
+	size_t	size;
+
+	if (n == 0)
+		return (1);
+	size = 0;
+	if (n < 0)
+		size++;
+	while (n != 0)
+	{
+		n = n / 10;
+		size++;
+	}
+	return (size);
+}
 
 char	*ft_itoa(int n)
 {
+	long	nl;
+	size_t	size;
+	char	*res;
+
+	nl = n;
+	size = ft_get_n_size(nl);
+	res = malloc(sizeof(char *) * (size + 1));
+	res[size] = '\0';
+	if (nl < 0)
+	{
+		res[0] = '-';
+		nl = -nl;
+	}
+	while (nl != 0)
+	{
+		res[size - 1] = (nl % 10) + '0';
+		size--;
+		nl /= 10;
+	}
+	return (res);
 }
