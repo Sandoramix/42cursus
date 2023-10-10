7e59034d7883d1583bf673bef7c5e7d9fc676cfc
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-10 19:58:07 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -13,4 +13,24 @@
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
+	char	*s1_p;
+	size_t	i;
+	size_t	j;
+	size_t	s2_len;
+
+	s1_p = (char *)s1;
+	s2_len = ft_strlen(s2);
+	if (s2_len == 0)
+		return (&s1_p[0]);
+	i = 0;
+	while (s1[i] && i < n)
+	{
+		j = 0;
+		while (s2[j] == s1[i + j])
+			j++;
+		if (j == s2_len)
+			return (&s1_p[i]);
+		i++;
+	}
+	return (NULL);
 }
