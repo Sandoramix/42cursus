0310e7776362200ebabb18fd490d3edfb6c214b7
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-11 23:04:38 +0200

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -16,7 +16,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	res_len;
 	char	*res;
 
-	res_len = ft_strlen(s1) + ft_strlen(s2);
-	res = calloc(s1);
 
+	res_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	res = ft_calloc(res_len, sizeof(char));
+	if (!res)
+		return (NULL);
+	ft_strlcat(res, s1, res_len);
+	ft_strlcat(res, s2, res_len);
+	return (res);
 }
