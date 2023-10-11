ee666565271aa6d7e96b7a98fc33346183595c88
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-11 23:04:29 +0200

    hotfix

--- ft_memmove.c
+++ ft_memmove.c
@@ -16,12 +16,14 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	size_t			i;
 	unsigned char	*res;
 
-	res = malloc(sizeof(char *) * n);
+	res = malloc(n);
 	i = -1;
 	while (++i < n)
 		res[i] = ((unsigned char *) src)[i];
 	i = -1;
 	while (++i < n)
 		((unsigned char *)dest)[i] = res[i];
+
+	free(res);
 	return (dest);
 }
