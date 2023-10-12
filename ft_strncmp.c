2c488f5d628a64adbae9f5d5258b890fdacf225a
Sandoramix <sandoramix.dev@gmail.com>
2023-10-13 00:11:08 +0200

    fix: `unsigned char *` comparations

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,11 +14,15 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	unsigned char	*s1p;
+	unsigned char	*s2p;
 
+	s1p = s1;
+	s2p = s2;
 	if (n == 0)
 		return (0);
 	i = 0;
-	while ((i < (n - 1)) && s2[i] == s1[i] && s2[i] && s1[i])
+	while ((i < n - 1) && s2p[i] == s1p[i] && s2p[i] && s1p[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return (s1p[i] - s2p[i]);
 }
