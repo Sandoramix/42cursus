249d63268d1cdc74ff7f2c451e7b3b730e94995a
Sandoramix <sandoramix.dev@gmail.com>
2023-10-13 00:10:24 +0200

    fix memmove: in case: - `d` and `s` are the same pointer; - `d` is inside the `s` memory range, moving form `s + n` till when `n` is 0 - if there's no overlapping, copying `s` into `d` from start to `n`

--- ft_memmove.c
+++ ft_memmove.c
@@ -11,18 +11,24 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t			i;
-	unsigned char	*res;
+	unsigned char	*d;
+	unsigned char	*s;
 
-	res = malloc(n);
-	i = -1;
-	while (++i < n)
-		res[i] = ((unsigned char *) src)[i];
-	i = -1;
-	while (++i < n)
-		((unsigned char *)dest)[i] = res[i];
-	free(res);
+	d = dest;
+	s = src;
+	if (d == s)
+		return (dest);
+	if (d >= s && d <= s + n)
+	{
+		d += n;
+		s += n;
+		while (n--)
+			*--d = *--s;
+		return (dest);
+	}
+	while (n--)
+		*d++ = *s++;
 	return (dest);
 }
