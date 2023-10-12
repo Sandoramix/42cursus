8337ee5928cc33a0ae9741f9b3a2f3dff440af37
Sandoramix <sandoramix.dev@gmail.com>
2023-10-13 01:54:11 +0200

    norme

--- ft_memmove.c
+++ ft_memmove.c
@@ -11,13 +11,13 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*d;
 	unsigned char	*s;
 
 	d = dest;
-	s = (unsigned char	*)src;
+	s = (unsigned char *)src;
 	if (d == s)
 		return (dest);
 	if (d >= s && d <= s + n)
