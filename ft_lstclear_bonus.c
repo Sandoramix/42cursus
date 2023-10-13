6183732a949b0100d35f9456e617c5428f26af7e
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-13 18:21:15 +0200

    final

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -13,4 +13,15 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*next;
+
+	if (!lst || !*lst || !del)
+		return ;
+	while (*lst)
+	{
+		next = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = next;
+	}
+	*lst = NULL;
 }
--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -13,4 +13,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
+	if (!lst || !del)
+		return ;
+	(*del)(lst->content);
+	free(lst);
 }
