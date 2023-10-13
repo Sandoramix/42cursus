57e967f5152fb75fdeb45cfa94f92257931ffd04
Sandoramix <sandoramix.dev@gmail.com>
2023-10-13 02:33:03 +0200

    cleanup

--- ft_calloc.c
+++ ft_calloc.c
@@ -15,11 +15,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*res;
 
-	if ((long long)size < 0 || (long long)nmemb <= 0)
-		return (NULL);
 	res = malloc(nmemb * size);
 	if (!res)
 		return (NULL);
-	ft_bzero(res, nmemb * size);
+	if (size > 0)
+		ft_bzero(res, nmemb * size);
 	return (res);
 }
