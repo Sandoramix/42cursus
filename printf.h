19c1faf40c7eb4987bd9188870ce6b075305c970
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-17 17:08:04 +0200

    initial commit

---
+++ printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odudniak <odudniak@student.42firenze.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 17:04:19 by odudniak          #+#    #+#             */
+/*   Updated: 2023/10/17 17:04:20 by odudniak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef PRINTF_H
+# define PRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif
