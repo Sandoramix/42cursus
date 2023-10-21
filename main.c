eaca934ad0bcdba1876558dd616391e7ba6f4064
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-21 16:52:57 +0200

    updated header

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: odudniak <odudniak@student.42firenze.      +#+  +:+       +#+        */
+/*   By: odudniak <odudniak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 17:51:10 by odudniak          #+#    #+#             */
-/*   Updated: 2023/10/17 17:51:20 by odudniak         ###   ########.fr       */
+/*   Updated: 2023/10/21 16:44:11 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+
 #include "printf.h"
 #include <stdio.h>
 
@@ -22,16 +22,12 @@ int	main(int ac, char **av)
 	if (ac < 2)
 		return (1);
 	val = ft_atoi(av[1]);
-	printf("%%d:\n\toriginal = %d\n\tmine = %s\n", val,
-		ft_itoa(val));
-	printf("%%x:\n\toriginal = %x\n\tmine = %s\n", val,
-		ft_itohex(val));
-	printf("%%p:\n\toriginal = %p\n\tmine = %s\n", test,
-		ft_getaddr(test));
+	printf("%%d:\n\toriginal = %d\n\tmine = %s\n", val, ft_itoa(val));
+	printf("%%x:\n\toriginal = %x\n\tmine = %s\n", val, ft_itohex(val));
+	printf("%%p:\n\toriginal = %p\n\tmine = %s\n", test, ft_getaddr(test));
 	// TODO WATCHOUT FOR ATOL
 	printf("%%u:\n\toriginal = %u\n\tmine = %s\n", (unsigned int)atol(av[1]),
 		ft_uitoa((unsigned int)atol(av[1])));
-
 	printf("% 3d", 3);
 	return (0);
 }
