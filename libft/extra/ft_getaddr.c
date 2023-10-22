8923fa514cab7673ffdafd3b8e09bc2240bc37db
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-22 15:41:25 +0200

    [fix] prevented memory leaks

--- libft/extra/ft_getaddr.c
+++ libft/extra/ft_getaddr.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_getaddr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: odudniak <odudniak@student.42firenze.      +#+  +:+       +#+        */
+/*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:52:12 by odudniak          #+#    #+#             */
-/*   Updated: 2023/10/21 10:51:13 by odudniak         ###   ########.fr       */
+/*   Updated: 2023/10/22 15:40:18 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_getaddr(void *p)
@@ -16,11 +17,14 @@ char	*ft_getaddr(void *p)
 	char			*res;
 	const uintptr_t	p_addr = (uintptr_t) p;
 	size_t			addr_len;
+	char			*itohex;
 
 	addr_len = ft_nbr_len(p_addr, 16) + 3;
 	res = ft_calloc(addr_len, sizeof(char));
 	res[0] = '0';
 	res[1] = 'x';
-	ft_strlcat(res, ft_itohex(p_addr), addr_len);
+	itohex = ft_itohex(p_addr);
+	ft_strlcat(res, itohex, addr_len);
+	free(itohex);
 	return (res);
 }
