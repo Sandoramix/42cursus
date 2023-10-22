3418c0bc130e12a2d25f3e852bbf82490129507f
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-22 18:28:27 +0200

    updated

--- main.c
+++ main.c
@@ -6,28 +6,42 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 16:53:58 by odudniak          #+#    #+#             */
-/*   Updated: 2023/10/21 16:54:01 by odudniak         ###   ########.fr       */
+/*   Updated: 2023/10/22 18:16:41 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 #include <stdio.h>
+#include <string.h>
 
 int	main(int ac, char **av)
 {
 	int		val;
-	char	*test;
+	char	*string;
+	// char	*itoa;
+	// char	*itohex;
+	// char	*getaddr;
+	// char	*uitoa;
 
-	test = "abcsd";
-	if (ac < 2)
-		return (1);
-	val = ft_atoi(av[1]);
-	printf("%%d:\n\toriginal = %d\n\tmine = %s\n", val, ft_itoa(val));
-	printf("%%x:\n\toriginal = %x\n\tmine = %s\n", val, ft_itohex(val));
-	printf("%%p:\n\toriginal = %p\n\tmine = %s\n", test, ft_getaddr(test));
-	// TODO WATCHOUT FOR ATOL
-	printf("%%u:\n\toriginal = %u\n\tmine = %s\n", (unsigned int)atol(av[1]),
-		ft_uitoa((unsigned int)atol(av[1])));
-	printf("% 3d", 3);
+	if (ac > 1)
+	{
+		val = ft_atoi(av[1]);
+		// itoa = ft_itoa(val);
+		// itohex = ft_itohex(val);
+		// getaddr = ft_getaddr(itohex);
+		// uitoa = ft_uitoa((unsigned int)val);
+		// printf("%%d:\n\toriginal = %d\n\tmine = %s\n", val, itoa);
+		// printf("%%x:\n\toriginal = %x\n\tmine = %s\n", val, itohex);
+		// printf("%%p:\n\toriginal = %p\n\tmine = %s\n", itohex, getaddr);
+		// printf("%%u:\n\toriginal = %u\n\tmine = %s\n", (uint32_t)val, uitoa);
+		// free(itoa);
+		// free(itohex);
+		// free(getaddr);
+		// free(uitoa);
+	}
+	string = ft_strdup("ab0c0de");
+	string = ft_strreplace(string, "0", " ciao ");
+	ft_printf("%s %% %d %u", string);
+	free(string);
 	return (0);
 }
