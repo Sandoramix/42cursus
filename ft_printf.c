669787bc2451ad1f5caee03f9570aa093a26f073
Sandoramix <sandoramix.dev@gmail.com>
2023-10-26 01:12:23 +0200

    WE'RE NEARLY HERE MY LORD

--- ft_printf.c
+++ ft_printf.c
@@ -10,22 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
-#include "libft.h"
-#include <stdio.h>
-
-static void	tmp_printlist(t_list *list)
-{
-	t_list	*l = list;
-	int i = 0;
-
-	while (l)
-	{
-		printf("[%d]: %s\n", i++, (char *)l->content);
-		l = l->next;
-	}
-}
-
+#include "ft_printf.h"
 
 typedef enum e_funtype
 {
@@ -35,59 +20,97 @@ typedef enum e_funtype
 	PF_FINT,
 	PF_UINT,
 	PF_FCHAR,
+	PF_FSTR,
 	PF_FHEX,
 	PF_FUPPERHEX,
 	PF_FPOINTER
 }	t_funtype;
 
-
-
-static void	pf_funswitch(char *s, va_list list, t_list *args)
+static size_t	pf_handlebonus(char *str, char *flag, t_funtype type)
 {
+	size_t	strlen;
 
+	if (!flag || !type)
+		return (0);
+	strlen = ft_strlen(str);
+	if (!str)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	ft_putstr_fd(str, 1);
+	free(str);
+	return (strlen);
 }
-static t_funtype	pf_get_argtype(char *s, va_list list, t_list arg)
-{
-	size_t	c_len;
 
-	c_len = ft_strlen(arg.content);
-	if (c_len < 2)
+static size_t	pf_funswitch(va_list list, char *flag, t_funtype type)
+{
+	char	*r;
+
+	r = NULL;
+	if (type == PF_FCHAR)
+	{
+		ft_putchar_fd((char)va_arg(list, int), 1);
+		return (1);
+	}
+	if (type == PF_FESCAPE)
+	{
+		ft_putchar_fd('%', 1);
+		return (1);
+	}
+	else if (type == PF_FINT)
+		r = ft_itoa(va_arg(list, int));
+	else if (type == PF_UINT)
+		r = ft_uitoa(va_arg(list, unsigned int));
+	else if (type == PF_FHEX)
+		r = ft_itohex(va_arg(list, unsigned int));
+	else if (type == PF_FUPPERHEX)
+		r = ft_strtoupper(ft_itohex(va_arg(list, unsigned int)));
+	else if (type == PF_FPOINTER)
+		r = ft_getaddr(va_arg(list, void *));
+	else
+		r = ft_strdup(va_arg(list, char *));
+	return (pf_handlebonus(r, flag, type));
+}
+
+static t_funtype	pf_get_argtype(char *flag)
+{
+	size_t		f_len;
+
+	f_len = ft_strlen(flag);
+	if (f_len < 2)
 		return (PF_FUNKNOWN);
-	if (arg.content[1] == '#')
+	if (flag[1] == '#')
 		return (PF_FCONVERSION);
-	if (ft_strchr("di", arg.content[c_len - 1]))
+	if (ft_strchr("di", flag[f_len - 1]))
 		return (PF_FINT);
-	if (arg.content[c_len - 1] == 'p')
+	if (flag[f_len - 1] == 'p')
 		return (PF_FPOINTER);
-	if (arg.content[c_len - 1] == 'x')
+	if (flag[f_len - 1] == 'x')
 		return (PF_FHEX);
-	if (arg.content[c_len - 1] == 'X')
+	if (flag[f_len - 1] == 'X')
 		return (PF_FUPPERHEX);
-	if (arg.content[c_len - 1] == 'u')
+	if (flag[f_len - 1] == 'u')
 		return (PF_UINT);
-	if (arg.content[c_len - 1] == '%')
+	if (flag[f_len - 1] == '%')
 		return (PF_FESCAPE);
+	if (flag[f_len - 1] == 'c')
+		return (PF_FCHAR);
+	if (flag[f_len - 1] == 's')
+		return (PF_FSTR);
 	return (PF_FUNKNOWN);
 }
 
-static void	pf_parseargs(char *s, va_list list, t_list *args)
+static void	pf_parseargs(const char *s, va_list list, size_t *len)
 {
-	while (args)
-	{
-		if (ft_strlen((char *)args->content) == 2)
-			pf_funswitch(s, list, args);
-		args = args->next;
-	}
-}
-
-static t_list	*pf_findargs(char *s)
-{
-	t_list	*res;
-	int		i;
-	int		start;
+	int			i;
+	int			start;
+	char		*flag;
+	int			print_idx;
+	int			parse_len;
 
 	i = -1;
-	res = NULL;
+	print_idx = 0;
 	while (s[++i])
 	{
 		if (s[i] == '%')
@@ -95,24 +118,25 @@ static t_list	*pf_findargs(char *s)
 			start = i;
 			while (ft_strchr(PF_ARGS_WHITELIST, s[++i]))
 				;
-			ft_lstadd_back(&res, ft_lstnew(ft_substr(s, start, i - start + 1)));
+			flag = ft_substr(s, start, i - start + 1);
+			write(1, s + print_idx, start - print_idx);
+			print_idx = i + 1;
+			parse_len = pf_funswitch(list, flag, pf_get_argtype(flag));
+			*len = (*len - (i - start + 1)) + parse_len;
+			free(flag);
 		}
 	}
-	return (res);
+	write(1, s + print_idx, ft_strlen(s) - print_idx);
 }
 
 int	ft_printf(const char *str, ...)
 {
-	char	*res;
-	va_list	list;
-	t_list	*args;
+	va_list		list;
+	size_t		res_len;
 
-	res = ft_strdup(str);
-	args = pf_findargs(res);
 	va_start(list, str);
-
-	tmp_printlist(args);
+	res_len = ft_strlen(str);
+	pf_parseargs(str, list, &res_len);
 	va_end(list);
-	free(res);
-	return (0);
+	return (res_len);
 }
