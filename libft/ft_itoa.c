45f788e521aeb46e65a43759410ab495d91944a6
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-31 14:38:35 +0100

    updated srcs & makefile

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,38 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: odudniak <odudniak@student.42firenze.it>   +#+  +:+       +#+        */
+/*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 00:16:47 by odudniak          #+#    #+#             */
-/*   Updated: 2023/10/21 10:50:36 by odudniak         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:12:26 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	ft_nbr_len(long long int n, int base_len)
-{
-	size_t	res;
-
-	res = 0;
-	if (n == 0)
-		return (1);
-	if (n < 0)
-		res++;
-	while (n != 0)
-	{
-		res++;
-		n /= base_len;
-	}
-	return (res);
-}
-
-static unsigned int	ft_iabs(int n)
-{
-	if (n < 0)
-		return (-n);
-	return (n);
-}
 
 char	*ft_itoa(int n)
 {
