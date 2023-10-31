9b05e8d4e2b443f2f3870a8827a2206354c663f1
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-10-31 14:38:05 +0100

    updated protos & docs

--- ft_printf.h
+++ ft_printf.h
@@ -6,210 +6,85 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 17:04:19 by odudniak          #+#    #+#             */
-/*   Updated: 2023/10/29 17:38:52 by odudniak         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:25:27 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include "libft.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
-# include "libft.h"
-
-# include <stdio.h>
+# include <stdbool.h>
 
 # define PF_ARGS_WHITELIST " .+-#0123456789"
 
-/**
- * @brief Like the original one
- *
- * @param str
- * @param ...
- * @return int
- */
-int		ft_printf(const char *str, ...);
+typedef enum e_funtype
+{
+	PF_FESCAPE,
+	PF_FUNKNOWN,
+	PF_FINT,
+	PF_UINT,
+	PF_FCHAR,
+	PF_FSTR,
+	PF_FHEX,
+	PF_FPOINTER
+}	t_funtype;
 
-//! EXTRA LIBFT
-//! MY FUNCTIONS
+typedef struct s_pfflag
+{
+	char		*flag;
+
+	char		*res;
+	int			reslen;
+
+	size_t		llen;
+	size_t		rlen;
+
+	t_funtype	ftype;
+
+	int			width;
+	int			prec;
+
+	bool		is_upper;
+	bool		has_prec;
+	bool		has_plus;
+	bool		has_minus;
+	bool		has_convertion;
+	bool		has_spaces;
+	bool		has_zeros;
+}	t_pfflag;
+
+
 /**
- * @brief Get the address of given pointer in hexadecimal letters
- * @param p memory area
- * @return The pointer's address in hexadecimal characters
- * @attention Uses: malloc
- */
-char	*ft_getaddr(void *p);
-/**
- * @brief Get the absolute value of a `int` number
+ * @brief Like the original one ;)
+ * Works well with: `-`-`-`-`+`-`#`-`.`
  *
- * @param n integer to convert
- * @return positive number
+ * @param str Format
+ * @param ... All necessary args
+ * @return Total printed chars.
  */
-t_uint	ft_iabs(int n);
+int			ft_printf(const char *str, ...);
 /**
- * @brief power of the `int` number
+ * @brief Yep we handle bonus here
  *
- * @param nb `int` number
- * @param power Power.
- * @return Returns the final value.
- * Returns `0` if `power` is negative
+ * @param flag Parsed flag
+ * @return size_t Return the final length of the formatted result.
  */
-int		ft_ipow(int nb, int power);
+size_t		pf_handlebonus(t_pfflag flag);
 /**
- * @brief Get the length of the string
+ * @brief Parse flag information
  *
- * @param s string
- * @return Length of the string as integer
+ * @param f flag's string
+ * @return t_pfflag the structure
  */
-int		ft_istrlen(const char *s);
+t_pfflag	pf_getflag(char *f);
+size_t		pf_handle_bonus_end(t_pfflag flag);
 /**
- * @brief print the number in given base
- *
- * @param n number to print
- * @param base the base in which to convert the number.
- * @return The converted number in specified base
- * @attention Uses: malloc
+ * @brief Print format of the result
+ * @param flag
+ * @return
  */
-char	*ft_itoa_base(int n, const char *base);
-/**
- * @brief Convert a number to hex
- * @param n number `int`
- * @return String of the hex number. (`0x`) excluded
- * @attention Uses: malloc
- */
-char	*ft_itohex(unsigned long n);
-/**
- * @brief Reverse the memory area of n elements.
- * `mem` is mutated.
- * @param mem memory area
- * @param len length of the area
- * @return pointer to `mem`
- */
-void	*ft_memrev(void *mem, size_t len);
-/**
- * @brief Get the total count of the number in specific base.
- * The minus (`-`) is included
- * @param n number to check
- * @param base_len len of the base (10, 16, etc.)
- * @return count of the final digits
- */
-int		ft_nbr_len(long long n, int base_len);
-/**
- * @brief Get the total count of the number in specific base.
- * @param n number to check
- * @param base_len len of the base (10, 16, etc.)
- * @return count of the final digits
- */
-int		ft_ulnbr_len(unsigned long n, int base_len);
-/**
- * @brief Find the index of first char `c` inside the string `s`
- *
- * @param s string
- * @param c char to find
- * @return Index of the first occurence of `c`, or `-1` if it's not present
- */
-int		ft_stridxofchar(const char *s, char c);
-/**
- * @brief FInd the index of first occurence of substring `find` inside `str`
- *
- * @param str string
- * @param find substring to find
- * @return Index of the first occurence of `find` inside `str`,
- * or `-1` if it's not present.
- */
-int		ft_stridxofstr(const char *str, char *find);
-/**
- * @brief Add left padding to the string with specified char if necessary.
- * @attention Original string is mutated.
- * @param s string to modify
- * @param c pad character
- * @param n total len with the string should have.
- * E.g. if `n` is 2, `c` is '0' and the s is `"4"` the result will be `"04"`
- * @return Copy of the string plus the added padding if necessary.
- * @attention Uses: malloc
- */
-char	*ft_strpadstart(char *s, char c, int n);
-/**
- * @brief Add right padding to the string with specified char if necessary.
- * @attention Original string is mutated.
- * @param s string to modify
- * @param c pad character
- * @param n total len with the string should have.
- * E.g. if `n` is 2, `c` is '0' and the s is `"4"` the result will be `"40"`
- * @return Copy of the string plus the added padding if necessary.
- * @attention Uses: malloc
- */
-char	*ft_strpadend(char *s, char c, int n);
-/**
- * @brief Update the string to uppercase.
- * @attention Original string is mutated.
- *
- * @param s string to return
- * @return The converted string.
- */
-char	*ft_strtoupper(char *s);
-/**
- * @brief Convert an `unsigned int` into a string
- * @param n unsigned int
- * @return The number as a string
- * @attention Uses: malloc.
- */
-char	*ft_uitoa(unsigned int n);
-/**
- * @brief Allocates (with malloc(3)) and returns a string
- * representing the integer received as an argument.
- *
- * @param n the unsigned integer to convert.
- * @return The string representing the integer.
- * NULL if the allocation fails.
- * @attention Uses: malloc
- */
-char	*ft_ultoa_base(unsigned long n, const char *base);
-/**
- * @brief Free the matrix!
- * @param mtx void ** -> mtx
- * @param len len
- * @return `NULL`
- * @attention Uses: free
- */
-void	*ft_mtxfree(void **mtx, size_t len);
-/**
- * @brief Find and replace the `first` substring of `str` with another string.
- * @attention Uses: malloc, free.
- * Unexpected behaviour may happen if the index is wrong.
- * `str` is freed.
- * @param str start string
- * @param start index where you want to start from.
- * @param find string to find
- * @param replace string to replace `find` with
- * @return the pointer to the new `str`. Or the same str if something went wrong.
- */
-char	*ft_strreplace_first(char *str, char *find, char *replace);
-/**
- * @brief Find and replace the `all` substrings of `str` with another string.
- * @attention Uses: malloc, free.
- * `str` is freed.
- * @param str start string
- * @param find string to find
- * @param replace string to replace `find` with
- * @return the pointer to the new `str`
- */
-char	*ft_strreplace(char *str, char *find, char *replace);
-/**
- * @brief Count how many times does `c` appear in `s`
- *
- * @param s string
- * @param c char
- * @return size_t
- */
-size_t	ft_strcount_c(const char *s, char c);
-/**
- * @brief Add a character to the end of a string
- * @attention Uses: malloc, free. Original string is mutated.
- * @param s string
- * @param c char to add
- * @return pointer to the start of modified string.
- * If there's been an issue with allocation, null is returned
- */
-char	*ft_strpushcar(char *s, char c);
+size_t		pf_handle_bonus_start(t_pfflag flag);
 #endif
