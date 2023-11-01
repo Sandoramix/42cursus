16f7e4ce80a9af67cf654b5ecce00455cdfdf961
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-11-01 14:58:18 +0100

    updated header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 17:04:19 by odudniak          #+#    #+#             */
-/*   Updated: 2023/10/31 15:28:02 by odudniak         ###   ########.fr       */
+/*   Updated: 2023/11/01 14:57:16 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,4 +85,71 @@ size_t		pf_handlebonus(t_pfflag flag);
 t_pfflag	pf_getflag(char *f);
 size_t		pf_handle_bonus_end(t_pfflag flag);
 size_t		pf_handle_bonus_start(t_pfflag flag);
+
+//! MY FUNCTIONS
+/**
+ * @brief Get the address of given pointer in hexadecimal letters
+ * @param p memory area
+ * @return The pointer's address in hexadecimal characters
+ * @attention Uses: malloc
+ */
+char	*ft_getaddr(void *p);
+/**
+ * @brief Get the length of the string
+ *
+ * @param s string
+ * @return Length of the string as integer
+ */
+int		ft_istrlen(const char *s);
+/**
+ * @brief Convert a number to hex
+ * @param n number `int`
+ * @return String of the hex number. (`0x`) excluded
+ * @attention Uses: malloc
+ */
+char	*ft_itohex(unsigned long n);
+/**
+ * @brief Get the total count of the number in specific base.
+ * @param n number to check
+ * @param base_len len of the base (10, 16, etc.)
+ * @return count of the final digits
+ */
+int		ft_ulnbr_len(unsigned long n, int base_len);
+/**
+ * @brief Add right padding to the string with specified char if necessary.
+ * @attention Original string is mutated.
+ * @param s string to modify
+ * @param c pad character
+ * @param n total len with the string should have.
+ * @param start Choose if the padding should be at start or end of the string
+ * E.g. if `n` is 2, `c` is '0' and the s is `"4"` the result will be `"40"`
+ * @return Copy of the string plus the added padding if necessary.
+ * @attention Uses: malloc
+ */
+char	*ft_strpad(char *s, char c, int n, bool start);
+/**
+ * @brief Update the string to uppercase.
+ * @attention Original string is mutated.
+ *
+ * @param s string to return
+ * @return The converted string.
+ */
+char	*ft_strtoupper(char *s);
+/**
+ * @brief Convert an `unsigned int` into a string
+ * @param n unsigned int
+ * @return The number as a string
+ * @attention Uses: malloc.
+ */
+char	*ft_uitoa(unsigned int n);
+/**
+ * @brief Allocates (with malloc(3)) and returns a string
+ * representing the integer received as an argument.
+ *
+ * @param n the unsigned integer to convert.
+ * @return The string representing the integer.
+ * NULL if the allocation fails.
+ * @attention Uses: malloc
+ */
+char	*ft_ultoa_base(unsigned long n, const char *base);
 #endif
