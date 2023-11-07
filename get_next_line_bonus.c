8fa9b1d34e4f6efc696217f26f326aaf35b5f646
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-11-07 16:32:19 +0100

    [bonus] implemented bonuses

---
+++ get_next_line_bonus.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 14:17:47 by odudniak          #+#    #+#             */
+/*   Updated: 2023/11/07 16:18:56 by odudniak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*gnl_read_until_needed(int fd, char *buffer)
+{
+	char		*rdata;
+	long		rlen;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	rdata = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!rdata)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	rlen = BUFFER_SIZE;
+	while (rlen == BUFFER_SIZE && ft_stridxof(rdata, '\n') == -1)
+	{
+		rlen = read(fd, rdata, BUFFER_SIZE);
+		if (rlen > 0)
+			buffer = my_strjoin(buffer, rdata, rlen);
+		else if (rlen <= 0 && ft_istrlen(buffer) == 0)
+		{
+			free(buffer);
+			buffer = NULL;
+		}
+	}
+	free(rdata);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[4096];
+	char		*res;
+	int			idxof;
+	char		*tmp;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	res = NULL;
+	buffer[fd] = gnl_read_until_needed(fd, buffer[fd]);
+	idxof = ft_stridxof(buffer[fd], '\n');
+	res = my_substr(buffer[fd], 0,
+			((int [2]){ft_istrlen(buffer[fd]), idxof})[idxof >= 0]);
+	tmp = my_substr(buffer[fd],
+			((int [2]){ft_istrlen(buffer[fd]), idxof + 1})[idxof >= 0],
+			ft_istrlen(buffer[fd]));
+	free(buffer[fd]);
+	buffer[fd] = my_substr(tmp, 0, ft_istrlen(tmp));
+	free(tmp);
+	return (res);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 14:17:53 by odudniak          #+#    #+#             */
+/*   Updated: 2023/11/07 16:19:01 by odudniak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+/**
+ * @brief Get the next line of given fd
+ *
+ * @param fd file descriptor
+ * @attention Here the buffer is a static array
+ * of maximum 4096 file descriptors (ulimit -n => 1024)
+ * @return Next line or `NULL` if reached the EOF / got an error
+ */
+char	*get_next_line(int fd);
+
+//? ****************************UTILS*****************************************
+/**
+ * @brief Get the length of given string.
+ * @param s pointer of the string
+ * @return Length of the string or `0` if `NULL` is passed.
+ */
+int		ft_istrlen(const char *s);
+/**
+ * @brief Allocate in heap memory N bytes and set each of them to `0`.
+ * @attention Uses: `malloc`
+ * @param n Number of elements.
+ * @param size size of each element.
+ * @return pointer to the created memory or `NULL` on allocation
+ * failure.
+ */
+void	*ft_calloc(size_t n, size_t size);
+/**
+ * @brief Concatenate two strings and `free` the first one.
+ * @attention Uses: `malloc` & `free`
+ * @param s1 First string which will be freed after the copy.
+ * @param s2 Second string
+ * @param s2_n Number of characters to copy from `s2`
+ * @return Concatenation of both strings, or `NULL` on allocation
+ * failure.
+ */
+char	*my_strjoin(char *s1, char *s2, size_t s2_n);
+/**
+ * @brief Create a substring of string `s` from `start` to `end` (included)
+ * @param s Main string
+ * @param start Start index
+ * @param end End index (included)
+ * @return Result of the string or `NULL` if length of `s` is zero.
+ */
+char	*my_substr(char *s, size_t start, size_t end);
+/**
+ * @brief Find the index of first occurence of `c` inside `str`
+ * @param str String to check
+ * @param c char to find
+ * @return index of the first occurence or `-1` if `c` was not found.
+ */
+int		ft_stridxof(char *str, char c);
+
+#endif
