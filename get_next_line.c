0a7f9202d0e6fe9a7835bb48fd3e9f8808b45988
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-11-07 16:09:06 +0100

    improved read performance

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 14:17:47 by odudniak          #+#    #+#             */
-/*   Updated: 2023/11/06 20:29:54 by odudniak         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:03:55 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ static char	*gnl_read_until_needed(int fd, char *buffer)
 		free(buffer);
 		return (NULL);
 	}
-	rlen = 1;
-	while (rlen > 0 && ft_stridxof(rdata, '\n') == -1)
+	rlen = BUFFER_SIZE;
+	while (rlen == BUFFER_SIZE && ft_stridxof(rdata, '\n') == -1)
 	{
 		rlen = read(fd, rdata, BUFFER_SIZE);
 		if (rlen > 0)
 			buffer = my_strjoin(buffer, rdata, rlen);
-		else if (rlen == 0 && ft_istrlen(buffer) == 0)
+		else if (rlen <= 0 && ft_istrlen(buffer) == 0)
 		{
 			free(buffer);
 			buffer = NULL;
