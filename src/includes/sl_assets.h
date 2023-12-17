962c977cf8b0dd19db397b01e53065f2d81dcb09
Oleksandr Dudniak <odudniak@student.42firenze.it>
2023-12-17 21:15:12 +0100

    added assets (WIP)

---
+++ src/includes/sl_assets.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sl_assets.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 13:06:04 by odudniak          #+#    #+#             */
+/*   Updated: 2023/12/17 14:38:40 by odudniak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SL_ASSETS_H
+# define SL_ASSETS_H
+
+# define SLA_COLLECTIBLE	"collectible.xpm"
+# define SLA_PLAYER			"player2.xpm"
+# define SLA_ENEMY			"enemy.xpm"
+# define SLA_FLOOR			"floor.xpm"
+# define SLA_WALL			"wall.xpm"
+# define SLA_EXIT			"exit.xpm"
+
+#endif
