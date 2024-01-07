29bb172e47d73511fa87a5a00da33462eff3dfab
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-01-07 17:32:45 +0100

    simple test main with all the implemented features

--- main.c
+++ main.c
@@ -3,160 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: odudniak <odudniak@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: odudniak <odudniak@student.42firenze.it>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/07 09:42:24 by odudniak          #+#    #+#             */
-/*   Updated: 2024/01/07 09:43:17 by odudniak         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/02 16:27:19 by odudniak          #+#    #+#             */
-/*   Updated: 2024/01/07 08:45:56 by odudniak         ###   ########.fr       */
+/*   Created: 2024/01/07 12:41:13 by odudniak          #+#    #+#             */
+/*   Updated: 2024/01/07 17:29:16 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
-
-typedef struct s_dllist
-{
-	int					*val;
-	struct s_dllist		*next;
-	struct s_dllist		*prev;
-}	t_dllist;
-
-
-void	*dll_clearlist(t_dllist **head)
-{
-	t_dllist	*head_tmp;
-	t_dllist	*curr;
-	t_dllist	*prev;
-
-	if (!head || !*head)
-		return (NULL);
-	prev = (*head)->prev;
-	head_tmp = (*head)->next;
-	free((*head)->val);
-	free(*head);
-	while (head_tmp && head_tmp != prev)
-	{
-		curr = head_tmp->next;
-		free(head_tmp->val);
-		free(head_tmp);
-		head_tmp = curr;
-	}
-	*head = NULL;
-	return (NULL);
-}
-
-void	dll_printlist(t_dllist *head)
-{
-	t_dllist	*tmp;
-	int			i;
-
-	i = -1;
-	if (head)
-		ft_printf("[%d]:\t%d\n", ++i, *(head->val));
-	tmp = head->next;
-	while (tmp)
-	{
-		ft_printf("[%d]:\t%d\n", ++i, *(tmp->val));
-		tmp = tmp->next;
-	}
-}
-
-t_dllist	*dll_new(int *val)
-{
-	t_dllist	*res;
-
-	res = malloc(sizeof(t_dllist));
-	if (!res)
-		return (NULL);
-	res->val = val;
-	res->next = NULL;
-	res->prev = NULL;
-	return (res);
-}
-
-int	dll_len(t_dllist **head)
-{
-	t_dllist	*res;
-	int			i;
-
-	if (!head)
-		return (0);
-	i = -1;
-	res = *head;
-	while (res)
-	{
-		i++;
-		res = res->next;
-	}
-	return (i);
-}
-
-t_dllist	*dll_getbyindex(t_dllist **head, int idx)
-{
-	t_dllist	*res;
-	int			i;
-
-	if (!head || !*head || idx < 0)
-		return (NULL);
-	i = -1;
-	res = *head;
-	while (res && ++i != idx)
-		res = res->next;
-	if (i == idx)
-		return (res);
-	return (NULL);
-}
-
-
-t_dllist	*dll_addhead(t_dllist **head, int *val)
-{
-	t_dllist	*node;
-	t_dllist	*head_tmp;
-
-	if (!val)
-		return (NULL);
-	node = dll_new(val);
-	if (!head || !*head)
-	{
-		*head = node;
-		return (*head);
-	}
-	head_tmp = *head;
-	*head = node;
-	(*head)->next = head_tmp;
-	(*head)->prev = head_tmp->prev;
-	return (*head);
-}
-
-t_dllist	*dll_addtail(t_dllist **head, int *val)
-{
-	t_dllist	*node;
-	t_dllist	*tmp;
-
-	if (!val)
-		return (NULL);
-	if (!head || !*head)
-		return (dll_addhead(head, val));
-	node = dll_new(val);
-	tmp = *head;
-	while (tmp->next != NULL || tmp->next != *head)
-		tmp = tmp->next;
-	node->prev = tmp;
-	if (tmp->next == *head)
-		node->next = *head;
-	tmp->next = node;
-	return (*head);
-}
 
 t_dllist	*tmp_populate(int ac, char **av)
 {
@@ -178,13 +32,36 @@ t_dllist	*tmp_populate(int ac, char **av)
 int	main(int ac, char **av)
 {
 	t_dllist	*stack_a;
+	t_dllist	*stack_b;
 
 	(void)ac;
 	(void)av;
 	stack_a = tmp_populate(ac, av);
+	stack_b = NULL;
+	//stack_a = tmp_populate(3, (char *[]){NULL, "1", "3"});
 	if (!stack_a)
 		return (1);
+	ft_printf("\nINITIAL STACK:\n");
 	dll_printlist(stack_a);
+	ft_printf("\t|\n\tROTATE OPERATION:\n");
+	dll_printlist(ps_rot(&stack_a));
+	ft_printf("\t|\n\tREVERSE ROTATE OPERATION:\n");
+	dll_printlist(ps_revrot(&stack_a));
+	ft_printf("\t|\n\tSWAP OPERATION:\n");
+	dll_printlist(ps_swap(&stack_a));
+	ft_printf("\t|\n\tPOP OPERATION:\n");
+	dll_printlist(dll_delhead(&stack_a));
+	ft_printf("\t|\n\tSHIFT OPERATION:\n");
+	dll_printlist(dll_deltail(&stack_a));
+	ft_printf("\t|\n\tPUSH FROM A TO B:\n");
+	ps_push(&stack_a, &stack_b);
+	ps_push(&stack_a, &stack_b);
+	ps_push(&stack_b, &stack_a);
+	ft_printf("\t\t|\n\t\tA:\n");
+	dll_printlist(stack_a);
+	ft_printf("\t\t|\n\t\tB:\n");
+	dll_printlist(stack_b);
 	dll_clearlist(&stack_a);
+	dll_clearlist(&stack_b);
 	return (0);
 }
