38ef1f3e2b9d972987053d5b3e160d37cc85d1d2
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-02-11 22:54:06 +0100

    tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:38:22 by odudniak          #+#    #+#             */
-/*   Updated: 2024/02/07 18:25:17 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/02/11 20:08:42 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 int	main(int ac, char **av)
 {
+	ft_printf("file_exists[%s]\n", ft_boolstr(file_exists("nonexistent", O_RDONLY)));
+	ft_printf("file_creation[%d]\n", file_open_or_create("nonexistent", O_RDONLY));
+	ft_printf("has_perm[%s]\n", ft_boolstr(file_hasperm("nonexistent", F_OK | R_OK | W_OK)));
 	if (ac < 5)
-		return (ft_perror("Error.\nInvalid number of arguments"), 1);
+		return (ft_perror("Error.\nInvalid number of arguments\n"), 1);
 	(void)ac;
 	(void)av;
 	return (0);
