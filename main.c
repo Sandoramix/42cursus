26637ca9008a93ec07bc361d61cc2ed08fb7f129
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-02-12 07:57:40 +0100

    file tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:38:22 by odudniak          #+#    #+#             */
-/*   Updated: 2024/02/11 20:08:42 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/02/12 07:56:59 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 
 int	main(int ac, char **av)
 {
-	ft_printf("file_exists[%s]\n", ft_boolstr(file_exists("nonexistent", O_RDONLY)));
-	ft_printf("file_creation[%d]\n", file_open_or_create("nonexistent", O_RDONLY));
-	ft_printf("has_perm[%s]\n", ft_boolstr(file_hasperm("nonexistent", F_OK | R_OK | W_OK)));
+	int			fd;
+
+	ft_printf("file_exists[%s]\n", ft_boolstr(file_exists("nonexistent")));
+	fd = file_open_or_create("nonexistent", O_RDWR);
+	ft_printf("file_creation[%d]\n", fd);
+	ft_printf("has_perm[%s]\n",
+		ft_boolstr(file_hasperm("nonexistent", F_OK | R_OK | W_OK)));
+	ft_fprintf(fd, "TEST %s\n", "write");
+	close(fd);
 	if (ac < 5)
 		return (ft_perror("Error.\nInvalid number of arguments\n"), 1);
 	(void)ac;
