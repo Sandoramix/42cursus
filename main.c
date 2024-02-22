90c2842494973dc7c227a6c87f00f95ea70cd44e
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-02-22 01:12:50 +0100

    nothing here

--- main.c
+++ main.c
@@ -6,45 +6,55 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:38:22 by odudniak          #+#    #+#             */
-/*   Updated: 2024/02/21 17:00:18 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/02/22 01:12:10 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex.h>
 
-void px_exit(t_pipex *data)
+void	px_exit(t_pipex *data)
 {
 	(void)data;
 }
 
-int	pipex(int ac, char **av, char **env)
+int	pipex(t_pipex *data)
 {
-	(void)ac;
-	(void)av;
-	(void)env;
+	(void)data;
 	return (0);
 }
 
+void	load_files_data(t_pipex *data, bool is_bonus)
+{
+	
+}
 
 #ifndef BONUS
 
 int	main(int ac, char **av, char **env)
 {
+	t_pipex	data;
+
+	data = (t_pipex){0};
+	data.ac = ac;
+	data.av = av;
 	ft_printf("MANDATORY\n");
 	if (ac != 5)
 		return (ft_perror("Error.\n %s (%d instead of 5)\n",
 				"Invalid number of arguments", ac), 1);
-	return (pipex(ac, av, env));
+	return (pipex(&data));
 }
 
 #else
-
+// TODO
 int	main(int ac, char **av, char **env)
 {
+	t_pipex	data;
+
+	data = (t_pipex){0};
 	ft_printf("BONUS\n");
 	if (ac < 5)
 		return (ft_perror("Error.\nInvalid number of arguments\n"), 1);
-	return (pipex(ac, av, env));
+	return (pipex(&data, ac, av, env));
 }
 
 #endif
--- src/includes/pipex.h
+++ src/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 18:19:17 by odudniak          #+#    #+#             */
-/*   Updated: 2024/02/21 17:02:23 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/02/21 21:26:30 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 
 typedef struct s_pipex
 {
+	// ORIGINAL AC/V VALUES. DO NOT FREE THESE
+	int			ac;
+	char		**av;
+
 	char		**env;
 
 	int			input_fd;
