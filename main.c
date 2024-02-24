ef0802484d529dc7fb9229afc298fe18d74d55ab
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-02-24 19:03:36 +0100

    the following of void

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:38:22 by odudniak          #+#    #+#             */
-/*   Updated: 2024/02/22 01:12:10 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/02/24 19:01:16 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,37 @@
 void	px_exit(t_pipex *data)
 {
 	(void)data;
+	lst_free(&data->env, free);
 }
+
 
 int	pipex(t_pipex *data)
 {
-	(void)data;
+	data->env = env_load(data->envp);
+	if (!data->env)
+		return (ft_fprintf(2, "ENV Error.\n"), px_exit(data), 1);
+	lst_printstr(data->env);
+	px_exit(data);
 	return (0);
 }
 
-void	load_files_data(t_pipex *data, bool is_bonus)
-{
+//void	load_files_data(t_pipex *data, bool is_bonus)
+//{
 	
-}
+//}
+
+
 
 #ifndef BONUS
 
-int	main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **envp)
 {
 	t_pipex	data;
 
 	data = (t_pipex){0};
 	data.ac = ac;
 	data.av = av;
+	data.envp = envp;
 	ft_printf("MANDATORY\n");
 	if (ac != 5)
 		return (ft_perror("Error.\n %s (%d instead of 5)\n",
--- src/includes/pipex.h
+++ src/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 18:19:17 by odudniak          #+#    #+#             */
-/*   Updated: 2024/02/21 21:26:30 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/02/24 18:31:04 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ typedef struct s_pipex
 	// ORIGINAL AC/V VALUES. DO NOT FREE THESE
 	int			ac;
 	char		**av;
+	char		**envp;
 
-	char		**env;
+	t_list		*env;
 
 	int			input_fd;
 	char		**input;
