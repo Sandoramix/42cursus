80f118433c68612ad5aa13eeb70afd66d89bc0a6
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-03-10 12:09:05 +0100

    cleanup

--- bonus.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/zsh
-source ~/.zshrc
-make debug-bonus
-valgrind --show-leak-kinds=all --leak-check=full ./pipex_bonus heredoc EOF "ls -l" "awk '{print \$1}'" out
-
-#--log-file=leak.log
--- mandatory.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/zsh
-source ~/.zshrc
-make
-valgrind --show-leak-kinds=all --leak-check=full ./pipex in "ls -l" "grep '4096 c' " out
