13b975728419c06d0653e4222ef1b59c6a3df67d
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-07-17 17:36:11 +0200

    ex02

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,157 @@
+#include "Account.hpp"
+#include <sstream>
+
+Account::Account(int initial_deposit)
+{
+	this->_amount = initial_deposit;
+	this->_accountIndex = _nbAccounts;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	_totalAmount += this->_amount;
+	_nbAccounts++;
+
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";" << "amount:" << this->_amount << ";" << "created" << std::endl;
+};
+
+Account::Account()
+{
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	this->_accountIndex = _nbAccounts;
+
+	_nbAccounts++;
+
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";" << "amount:" << this->_amount << ";" << "created" << std::endl;
+};
+
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";" << "amount:" << this->_amount << ";" << "closed" << std::endl;
+}
+
+
+// STATIC VARIABLES------------------------------
+int    Account::_nbAccounts = 0;
+int    Account::_totalAmount = 0;
+int    Account::_totalNbDeposits = 0;
+int    Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts()
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount()
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits()
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals()
+{
+	return _totalNbWithdrawals;
+}
+//-----------------------------------------------
+
+static std::string padTime(int val)
+{
+	std::string res;
+	if (val < 10)
+		res.append("0");
+	std::ostringstream ss;
+	ss << val;
+	res.append(ss.str());
+	return res;
+}
+
+void Account::_displayTimestamp(void)
+{
+	std::time_t time = std::time(0);
+	std::tm *now = localtime(&time);
+
+	std::cout << "["
+			  <<  (now->tm_year + 1900) << padTime(now->tm_mon + 1) << padTime(now->tm_mday)
+			  << "_" << padTime(now->tm_hour) << padTime(now->tm_min) << padTime(now->tm_sec)
+			  << "] ";
+}
+
+void Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";" << "p_amount:" << _amount << ";";
+	if (deposit < 0)
+	{
+		std::cout << "deposit:refused" << std::endl;
+		return;
+	}
+
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+
+	std::cout << "deposit:" << deposit << ";"
+			  << "amount:" << this->_amount << ";"
+			  << "nb_deposits:" << this->_nbDeposits << ";"
+			  << std::endl;
+}
+
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";" << "p_amount:" << _amount << ";";
+	if (withdrawal < 0 || withdrawal >= this->_amount)
+	{
+		std::cout << "withdrawal:refused" << std::endl;
+		return false;
+	}
+	this->_nbWithdrawals++;
+	this->_amount -= withdrawal;
+
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	_totalNbDeposits++;
+
+	std::cout << "withdrawal:" << withdrawal << ";"
+			  << "amount:" << this->_amount << ";"
+			  << "nb_withdrawals:" << this->_nbWithdrawals << ";"
+			  << std::endl;
+	return true;
+}
+
+void Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";"
+			  << "total:" << _totalAmount << ";"
+			  << "deposits:" << _totalNbDeposits << ";"
+			  << "withdrawals:" << _totalNbWithdrawals
+			  << std::endl;
+}
+
+int Account::checkAmount(void) const
+{
+	return this->_amount;
+}
+
+void Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";" << "amount:" << this->_amount << ";"
+			  << "deposits:" << this->_nbDeposits << ";"
+			  << "withdrawals:" << this->_nbWithdrawals
+			  << std::endl;
+}
+
+//-----------------------------------------------
--- cpp00/ex02/Account.hpp
+++ cpp00/ex02/Account.hpp
@@ -12,58 +12,59 @@
 #ifndef __ACCOUNT_H__
 # define __ACCOUNT_H__
 
+#include <ctime>
+#include <iostream>
+#include <cstddef>
+#include <string>
+
 // ************************************************************************** //
 //                               Account Class                                //
 // ************************************************************************** //
 
-class Account {
-
-
+class Account
+{
 private:
 
-	static int	_nbAccounts;
-	static int	_totalAmount;
-	static int	_totalNbDeposits;
-	static int	_totalNbWithdrawals;
+	static int _nbAccounts;
+	static int _totalAmount;
+	static int _totalNbDeposits;
+	static int _totalNbWithdrawals;
 
-	static void	_displayTimestamp( void );
+	static void _displayTimestamp(void);
 
-	int				_accountIndex;
-	int				_amount;
-	int				_nbDeposits;
-	int				_nbWithdrawals;
+	void _stamp(void) const;
 
-	Account( void );
+	int _accountIndex;
+	int _amount;
+	int _nbDeposits;
+	int _nbWithdrawals;
+
+	Account(void);
+
 public:
-	typedef Account		t;
+	typedef Account t;
 
-	static int	getNbAccounts( void );
-	static int	getTotalAmount( void );
-	static int	getNbDeposits( void );
-	static int	getNbWithdrawals( void );
-	static void	displayAccountsInfos( void );
+	static int getNbAccounts(void);
 
-	Account( int initial_deposit );
-	~Account( void );
+	static int getTotalAmount(void);
 
-	void	makeDeposit( int deposit );
-	bool	makeWithdrawal( int withdrawal );
-	int		checkAmount( void ) const;
-	void	displayStatus( void ) const;
+	static int getNbDeposits(void);
+
+	static int getNbWithdrawals(void);
+
+	static void displayAccountsInfos(void);
+
+	Account(int initial_deposit);
+
+	~Account(void);
+
+	void makeDeposit(int deposit);
+
+	bool makeWithdrawal(int withdrawal);
+
+	int checkAmount(void) const;
+
+	void displayStatus(void) const;
 };
 
-Account::Account(int initial_deposit)
-	: _amount(initial_deposit)
-{}
-
-
-
-// ************************************************************************** //
-// vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //
-// -*- indent-tabs-mode:t;                                                   -*-
-// -*- mode: c++-mode;                                                       -*-
-// -*- fill-column: 75; comment-column: 75;                                  -*-
-// ************************************************************************** //
-
-
-#endif /* __ACCOUNT_H__ */
+#endif
