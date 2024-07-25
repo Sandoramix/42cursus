358b6418a4ca0f83befa887191bf1f7c5fcc68a7
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-07-25 09:28:14 +0200

    cpp03 ex00

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,90 @@
+#include "ClapTrap.hpp"
+#include <ostream>
+
+
+ClapTrap::ClapTrap() : name("DEFAULT"), hitPoints(10), energyPoints(10), attackDamage(0)
+{
+
+}
+
+ClapTrap::ClapTrap(std::string name) : name(name), hitPoints(10), energyPoints(10), attackDamage(0)
+{
+	std::cout << "[DEFAULT]\tconstructor\tname: " << this->name << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap &clapTrap)
+{
+	this->name = clapTrap.name;
+	this->hitPoints = clapTrap.hitPoints;
+	this->energyPoints = clapTrap.energyPoints;
+	this->attackDamage = clapTrap.attackDamage;
+	std::cout << "[COPY]\t\tconstructor\tname: " << this->name << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "[DESTRUCTOR]\t\tname: " << this->name << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(ClapTrap &ct)
+{
+	std::string oldname = this->name;
+	this->name = ct.name;
+	this->hitPoints = ct.hitPoints;
+	this->energyPoints = ct.energyPoints;
+	this->attackDamage = ct.attackDamage;
+	std::cout << "[COPY]\tassignment\told name: " << oldname << "\tname: " << this->name << std::endl;
+	return *this;
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (!this->hitPoints)
+	{
+		std::cout << "ClapTrap " << this->name << " tried to attack " << target << "." << std::endl
+				  << "\tSadly enough... dead can't hurt anyone" << std::endl;
+		return;
+	}
+	if (!this->energyPoints)
+	{
+		std::cout << "ClapTrap " << this->name << " planned to attack " << target
+				  << " but got very lazy and forgot to do so" << std::endl;
+		return;
+	}
+	std::cout << "ClapTrap " << this->name
+			  << " attacks " << target << " by causing " << this->attackDamage << " damage points."
+			  << std::endl << "\tBut we don't know who " << target << " is, so it wont ever be hurt."
+			  << std::endl;
+	this->energyPoints--;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (!this->hitPoints)
+	{
+		std::cout << "Have mercy over " << this->name << ". Can't you see? It's already on the floor ~.~" << std::endl;
+		return;
+	}
+	std::cout << "ClapTrap " << this->name << " took " << amount << " damage." << std::endl;
+	this->hitPoints -= amount > this->hitPoints ? this->hitPoints : amount % (this->hitPoints + 1);
+	std::cout << "\tRemaining hitPoints: " << this->hitPoints << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (!this->energyPoints)
+	{
+		std::cout << "ClapTrap " << this->name << " tried to repair itself for " << amount << " hitPoints..." << std::endl
+				  << "\tBut it didn't considerate that he was out of energy points (RIP)" << std::endl;
+		return;
+	}
+	std::cout << "ClapTrap " << this->name << " repaired itself for " << amount << " hitPoints." << std::endl;
+	this->hitPoints += amount;
+	this->energyPoints--;
+	std::cout << "\tRemaining hitPoints: " << this->hitPoints << std::endl;
+}
+
+void ClapTrap::setName(std::string newName)
+{
+	this->name = newName;
+}
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energyPoints;
+	unsigned int attackDamage;
+
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(ClapTrap &clapTrap);
+	~ClapTrap();
+
+	ClapTrap &operator=(ClapTrap &ct);
+
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+	void setName(std::string newName);
+};
+
+
+#endif //CLAPTRAP_HPP
