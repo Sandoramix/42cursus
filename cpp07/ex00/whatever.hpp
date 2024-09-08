006aaee1fc5b633d22e9df0f18d44ca9690fc92a
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-09-08 18:57:19 +0200

    reorganized includes

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -1,7 +1,7 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-template<class T>
+template<typename T>
 void swap(T &val1, T &val2)
 {
 	T tmp = val1;
@@ -9,13 +9,13 @@ void swap(T &val1, T &val2)
 	val2 = tmp;
 }
 
-template<class T>
+template<typename T>
 T min(const T &val1, const T &val2)
 {
 	return val1 < val2 ? val1 : val2;
 }
 
-template<class T>
+template<typename T>
 T max(const T &val1, const T &val2)
 {
 	return val1 > val2 ? val1 : val2;
