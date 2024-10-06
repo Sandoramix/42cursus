9950ab3d36dc4ee56936d0dbcb0c3c938621dec8
Sandoramix <sandoramix.dev@gmail.com>
2024-10-06 20:15:02 +0200

    [update] extend WordPress Docker setup: add environment variables, health check, entrypoint script, and configure dependencies

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+WP_PATH=/var/www/html
+
+if ! wp core is-installed --path=${WP_PATH}; then
+	wp core download --path="$WP_PATH"
+
+	if [ ! -e "$WP_PATH/wp-config.php" ]; then
+		wp config create --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}" --path="$WP_PATH"
+	fi
+
+	if ! wp db check --path="$WP_PATH"; then
+		wp db create --path="$WP_PATH"
+	fi
+
+	wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --path="$WP_PATH"
+else
+	echo "WordPress is already installed."
+fi
+
+"$@"
