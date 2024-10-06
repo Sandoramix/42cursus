d5f3cdb3a43833df76a27902923c7878b0d3f62b
Sandoramix <sandoramix.dev@gmail.com>
2024-10-06 20:18:50 +0200

    [update] modify .env.example and entrypoint.sh: add secondary user and improve WordPress installation script

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -14,6 +14,8 @@ if ! wp core is-installed --path=${WP_PATH}; then
 	fi
 
 	wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --path="$WP_PATH"
+
+	wp user create "${WORDPRESS_SECONDARY_USER}" "${WORDPRESS_SECONDARY_USER_EMAIL}" --user_pass="${WORDPRESS_SECONDARY_USER_PASSWORD}" --role=author --path="$WP_PATH"
 else
 	echo "WordPress is already installed."
 fi
