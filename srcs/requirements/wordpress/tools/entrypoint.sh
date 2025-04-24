cde387fc9c90c23e21767a5a741be9a0c80db41a
Sandoramix <sandoramix.dev@gmail.com>
2025-04-24 15:40:27 +0200

    update: enhance WordPress setup by increasing memory limit and adding --allow-root to commands in entrypoint script for Docker compatibility

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 WP_PATH=/var/www/html
+cd "$WP_PATH"
 
-if ! wp core is-installed 2>/dev/null --path=${WP_PATH}; then
-	wp core download --path="$WP_PATH"
+if ! wp core is-installed 2>/dev/null; then
+	wp core download --allow-root --force
 
 	if [ ! -e "$WP_PATH/wp-config.php" ]; then
-		wp config create --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}" --path="$WP_PATH"
+		wp config create --allow-root --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}"
 	fi
 
-	if ! wp db check --path="$WP_PATH"; then
-		wp db create --path="$WP_PATH"
+	if ! wp db check; then
+		wp db create --allow-root
 	fi
 
 	WORDPRESS_SITE_URL=${WORDPRESS_SITE_PROTOCOL}${WORDPRESS_SITE_DOMAIN}$([ "${WORDPRESS_SITE_PORT}" != "443" ] && echo ":${WORDPRESS_SITE_PORT}")
 
-	wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --path="$WP_PATH"
+	wp core install --allow-root --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}"
 
-	wp user create "${WORDPRESS_SECONDARY_USER}" "${WORDPRESS_SECONDARY_USER_EMAIL}" --user_pass="${WORDPRESS_SECONDARY_USER_PASSWORD}" --role=author --path="$WP_PATH"
+	wp user create --allow-root "${WORDPRESS_SECONDARY_USER}" "${WORDPRESS_SECONDARY_USER_EMAIL}" --user_pass="${WORDPRESS_SECONDARY_USER_PASSWORD}" --role=author
 else
 	echo "WordPress is already installed."
 fi
